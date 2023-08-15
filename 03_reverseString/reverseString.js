ed2416e4e8707c8a44bc2d52e0882d93c4940988
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-15 12:15:27 -0500

    03 exercise reverseString()
    Takes string and reverses it with split and join, also skips empty string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if (word === "") {
+        return "";
+    } else {
+        let wordArray = word.split("");
+        let reversedWord = wordArray.reverse();
 
+        return reversedWord.join("");
+    }
 };
 
 // Do not edit below this line
