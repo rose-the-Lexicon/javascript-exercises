edb7cb73246dfea3ae8176bc483588f1ec49d68a
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-15 11:48:00 -0500

    Completed 01 and 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatLimit) {
+    let repeatedStr = ""
+    
+    if (repeatLimit >= 0) {
+        for (let i = 0; i < repeatLimit; i++){
+            repeatedStr = repeatedStr.concat(word);
+        }
+        return repeatedStr;
+    
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
