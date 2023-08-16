1f137f80d3e59f2fe73a3a85d34c04f1878665e2
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-16 08:03:04 -0500

    Add sum of all numbers between two numbers
    Used for loop to add all. Also checked for user input errors.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumOfAll = 0;
+    let numStart = 0;
+    let numEnd = 0;
+    
+    //checking for user error
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        if (num1 < 0 || num2 < 0) {
+            return "ERROR";
+        } else if (num1 > num2) {
+            numEnd = num1;
+            numStart = num2;
+        } else {
+            numEnd = num2;
+            numStart = num1;
+        }
+    } else {
+        return "ERROR";
+    }
 
+    for (let i = numStart; i <= numEnd; i++) {
+        sumOfAll += i;
+    }
+    
+    return sumOfAll;
 };
 
 // Do not edit below this line
