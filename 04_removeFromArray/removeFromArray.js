722daeba211d47b9e5735146834be6971e08cab7
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-15 19:18:08 -0500

    RemoveFromArray finished with rest arguments
    used forloop, splicex, and index of to remove array item

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayToEdit, ...argumentsRemoved) {
+    for (const arg of argumentsRemoved) {
+        if (arrayToEdit.includes(arg)) {
+            let index = arrayToEdit.indexOf(arg);
+            arrayToEdit.splice(index, 1);
+        } else {
+            continue;
+        }
+    }
+    return arrayToEdit;
 };
 
 // Do not edit below this line
