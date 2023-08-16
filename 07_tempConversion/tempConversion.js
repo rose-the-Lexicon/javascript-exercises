b814185feb4a0bcfc0a08a23d2dfc6dcdb7be98e
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-16 15:43:55 -0500

    Made temp conversion functions
    Made them round to first decimal point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return Math.round(((tempF - 32) * .5556) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempF) {
+  return Math.round(((tempF * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
