83cd489ffe29a7a97fd39d89e06f33b189772b02
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-16 15:45:14 -0500

    Fixed typo for c to f

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const convertToCelsius = function(tempF) {
   return Math.round(((tempF - 32) * .5556) * 10) / 10;
 };
 
-const convertToFahrenheit = function(tempF) {
-  return Math.round(((tempF * 1.8) + 32) * 10) / 10;
+const convertToFahrenheit = function(tempC) {
+  return Math.round(((tempC * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
